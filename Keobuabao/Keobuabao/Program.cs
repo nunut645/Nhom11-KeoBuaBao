483fd8410cfedcf60e93f5d994d3b77b444ed762
Lê Thị Thu Trang <trangltt1071@ut.edu.vn>
2025-09-01 16:44:30 +0700

    Nhận dữ liệu

--- Keobuabao/Keobuabao/Program.cs
+++ Keobuabao/Keobuabao/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class GameServer
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("Tro choi dang chuan bi...");
+
+        // Chờ client 1
+        Console.WriteLine("Dang cho nguoi choi vao 1...");
+        TcpClient client1 = server.AcceptTcpClient();
+        NetworkStream ns1 = client1.GetStream();
+        Console.WriteLine("Nguoi choi 1 da vao phong");
+
+        // Chờ client 2
+        Console.WriteLine("Dang cho nguoi choi 2...");
+        TcpClient client2 = server.AcceptTcpClient();
+        NetworkStream ns2 = client2.GetStream();
+        Console.WriteLine("Nguoi choi 2 da vao phong");
+
+        // Nhận dữ liệu từ client 1
+        byte[] buffer = new byte[1024];
+        int bytesRead1 = ns1.Read(buffer, 0, buffer.Length);
+        string player1 = Encoding.UTF8.GetString(buffer, 0, bytesRead1);
+
+        // Nhận dữ liệu từ client 2
+        int bytesRead2 = ns2.Read(buffer, 0, buffer.Length);
+        string player2 = Encoding.UTF8.GetString(buffer, 0, bytesRead2);
+
+        Console.WriteLine($"Nguoi choi 1: {player1}");
+        Console.WriteLine($"Nguoi choi 2: {player2}");
