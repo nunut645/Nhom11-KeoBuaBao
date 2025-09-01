6b486a1addc07bc662ec92f475cd242b346f3856
Lê Thị Thu Trang <trangltt1071@ut.edu.vn>
2025-09-01 16:25:48 +0700

    Nhận dữ liệu

--- Keobuabao/Client/Program.cs
+++ Keobuabao/Client/Program.cs
@@ -12,7 +12,7 @@ class GameServer
         Console.WriteLine("Tro choi dang chuan bi...");
 
         // Chờ client 1
-        Console.WriteLine("Dang cho nguoi choi 1...");
+        Console.WriteLine("Dang cho nguoi choi vao 1...");
         TcpClient client1 = server.AcceptTcpClient();
         NetworkStream ns1 = client1.GetStream();
         Console.WriteLine("Nguoi choi 1 da vao phong");
@@ -34,34 +34,3 @@ class GameServer
 
         Console.WriteLine($"Nguoi choi 1: {player1}");
         Console.WriteLine($"Nguoi choi 2: {player2}");
-
-        // Xử lý kết quả
-        string result = GetResult(player1, player2);
-
-        // Gửi kết quả cho cả 2
-        byte[] resBytes = Encoding.UTF8.GetBytes(result);
-        ns1.Write(resBytes, 0, resBytes.Length);
-        ns2.Write(resBytes, 0, resBytes.Length);
-
-        client1.Close();
-        client2.Close();
-        server.Stop();
-    }
-
-    static string GetResult(string p1, string p2)
-    {
-        string[] parts1 = p1.Split(':');
-        string[] parts2 = p2.Split(':');
-        string name1 = parts1[0], choice1 = parts1[1];
-        string name2 = parts2[0], choice2 = parts2[1];
-
-        if (choice1 == choice2)
-            return "Hoa!";
-        else if ((choice1 == "keo" && choice2 == "bao") ||
-                 (choice1 == "bua" && choice2 == "keo") ||
-                 (choice1 == "bao" && choice2 == "bua"))
-            return $"{name1} win!";
-        else
-            return $"{name2} win!";
-    }
-}
